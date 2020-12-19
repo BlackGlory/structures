dfd0c237753f4030bb5de11bf311b96170649216
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-12-19 20:30:42 +0800

    feat: add Queue

---
+++ README.md
@@ -0,0 +1,11 @@
+# structures
+
+Common structures.
+
+## Install
+
+```sh
+npm install --save @blackglory/structures
+# or
+yarn add @blackglory/structures
+```
---
+++ rollup.config.js
@@ -0,0 +1,67 @@
+import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+
+const UMD_NAME = 'Structures'
+
+function createOptions({ directory, target }) {
+  return [
+    {
+      input: 'src/index.ts'
+    , output: createOutput('index')
+    , plugins: [
+        typescript({ target })
+      ]
+    }
+  , {
+      input: 'src/index.ts'
+    , output: createMinification('index')
+    , plugins: [
+        typescript({ target })
+      , terser()
+      ]
+    }
+  ]
+
+  function createOutput(name) {
+    return [
+      {
+        file: `dist/${directory}/${name}.mjs`
+      , format: 'es'
+      , sourcemap: true
+      }
+    , {
+        file: `dist/${directory}/${name}.umd.js`
+      , format: 'umd'
+      , name: UMD_NAME
+      , sourcemap: true
+      }
+    ]
+  }
+  k
+  function createMinification(name) {
+    return [
+      {
+        file: `dist/${directory}/${name}.min.mjs`
+      , format: 'es'
+      , sourcemap: true
+      }
+    , {
+        file: `dist/${directory}/${name}.umd.min.js`
+      , format: 'umd'
+      , name: UMD_NAME
+      , sourcemap: true
+      }
+    ]
+  }
+}
+
+export default [
+  ...createOptions({
+    directory: 'es2015'
+  , target: 'ES2015'
+  })
+, ...createOptions({
+    directory: 'es2018'
+  , target: 'ES2018'
+  })
+]
