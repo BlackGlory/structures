b01dfe182f1062fbcb4ade21580fe8dc6846d24d
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-09-20 16:25:09 +0000

    perf: improve benchmark

--- benches/benchmark.ts
+++ benches/benchmark.ts
@@ -2,10 +2,26 @@ import { go } from '@blackglory/go'
 import { BitSet } from '..'
 import { Benchmark } from 'extra-benchmark'
 
-const benchmark = new Benchmark('BitSet')
+const benchmark = new Benchmark('BitSet', {
+  warmUps: 10000
+, runs: 10000
+})
 
 go(async () => {
-  benchmark.addCase('has', () => {
+  benchmark.addCase('Set.has', () => {
+    const set = new Set<number>()
+    for (let i = 0; i < 10000; i += 2) {
+      set.add(i)
+    }
+
+    return () => {
+      for (let i = 10000; i--;) {
+        set.has(i)
+      }
+    }
+  })
+
+  benchmark.addCase('BitSet.has', () => {
     const set = new BitSet()
     for (let i = 0; i < 10000; i += 2) {
       set.add(i)
@@ -18,7 +34,21 @@ go(async () => {
     }
   })
 
-  benchmark.addCase('add', () => {
+  benchmark.addCase('Set.add', () => {
+    return () => {
+      const set = new Set<number>()
+
+      for (let i = 0; i < 10000; i += 2) {
+        set.add(i)
+      }
+
+      for (let i = 10000; i--;) {
+        set.add(i)
+      }
+    }
+  })
+
+  benchmark.addCase('BitSet.add', () => {
     return () => {
       const set = new BitSet()
 
@@ -32,7 +62,21 @@ go(async () => {
     }
   })
 
-  benchmark.addCase('delete', () => {
+  benchmark.addCase('Set.delete', () => {
+    const set = new Set<number>()
+
+    return () => {
+      for (let i = 0; i < 10000; i += 2) {
+        set.add(i)
+      }
+
+      for (let i = 10000; i--;) {
+        set.delete(i)
+      }
+    }
+  })
+
+  benchmark.addCase('BitSet.delete', () => {
     const set = new BitSet()
 
     return () => {
